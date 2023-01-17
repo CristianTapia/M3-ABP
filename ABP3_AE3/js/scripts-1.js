7154d1b6204aea2434f1f842417ad67b57d5d698
Cristian <ctapia.macaya@gmail.com>
2023-01-17 01:36:40 -0300

    Problema 1 resuelto

--- ABP3_AE3/js/scripts-1.js
+++ ABP3_AE3/js/scripts-1.js
@@ -1,15 +1,10 @@
 function showDiv() {
-    let div = document.getElementById("show").style.display;
-    let value = document.getElementById("num").value;
-    if (value <= 100 && value > 1) {
-        document.getElementById("show").innerHTML = "<p>El número " + value + " tiene opciones de ganar";
-        if ((value > 5 && value < 15) || value == 70 || value == 55) {
-            document.getElementById("show").innerHTML = "<p>El número " + value + " tiene más opciones de ganar";
-        } 
+    let valueLength = document.getElementById("num").value;
+    let array = [];
+    for (let i = 0; i < valueLength; i++) {
+        array.push(i + 1);
+        array[i + 1] = array[i] * 2;
+        console.log(array)
     }
-    else {
-        document.getElementById("show").innerHTML = "<p>El número " + value  + " no tiene opciones de ganar";
-    }
-}
-
-
+    document.getElementById("show").innerHTML = `<div>El arreglo generado: <br>[${array}]</div>`;
+} 
