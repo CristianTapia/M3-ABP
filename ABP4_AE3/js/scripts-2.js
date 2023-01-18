c072f52b2ca55cdb4182518a531c554442581058
Cristian <ctapia.macaya@gmail.com>
2023-01-18 16:28:21 -0300

    Problema 2 resuelto ABP4_AE3

---
+++ ABP4_AE3/js/scripts-2.js
@@ -0,0 +1,24 @@
+// Crear arreglo aleatorio
+
+function checkNumber() {
+    let number = document.getElementById("num").value;
+    let isPrime = true;
+    if (number > 1) {
+        for (let i = 2; i < (number - 1); i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime == true) {
+            document.getElementById("show").innerHTML = `El número ${number} es primo`;
+        } else {
+            document.getElementById("show").innerHTML = `El número ${number} no es primo`;
+        }   
+    } if (number == 0) {
+        document.getElementById("show").innerHTML = `El número ${number} no se puede determinar`;
+    } if (number == 1) {
+        document.getElementById("show").innerHTML = `El número ${number} no se considera primo`;
+    }  
+}
+
