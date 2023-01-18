82c3b37902675253ee98e12b5bc2b1d3b8e2c316
Cristian <ctapia.macaya@gmail.com>
2023-01-17 21:54:19 -0300

    Problemas resueltos a la mitad

--- ABP3_AE3/js/scripts-4.js
+++ ABP3_AE3/js/scripts-4.js
@@ -1,30 +1,79 @@
 // Crear arreglo aleatorio
+var array = [];
 
 function createArray() {
     var arraySize = document.getElementById("num").value;
-    let array = [];
     if (arraySize > 0 && arraySize <= 100) {
         for (let i = 0; i < arraySize; i++) {
             array.push(Math.floor(Math.random()*100));
         }
-        let maxNumber = Math.max(...array);
         document.getElementById("show").innerHTML = `Arreglo generado: <br><br>[${array}]`;
         document.getElementById("search-box").style.display = "block";
     } else {
         document.getElementById("show").innerHTML = `El número no es aceptable`;
     }
-    
 }
 
 function searchNumber() {
+    console.log(array)
+    var arraySize = document.getElementById("num").value;
     let searchNum = document.getElementById("search").value;
-    if (searchNum > 0 && searchNum <= 100) {
+    if (searchNum >= 0 && searchNum <= 100) {
         for (let i = 0; i < arraySize; i++) {
-            if (i == searchNum) {
-                document.getElementById("show-element").innerHTML = `El número ${searchNum} está en la posición`;
+            if (searchNum == array[i]) {
+                let match= true;
+            }
+            if (match == true) {
+                document.getElementById("show-element").innerHTML = `El número ${searchNum} está en la posición ${i}`;
             }
         }
     } else {
-        document.getElementById("show-element").innerHTML = `El número ${searchNum} no está en el arreglo`;
+        document.getElementById("show-element").innerHTML = `El número ${searchNum} no es válido`;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function createArray() {
+//     var arraySize = document.getElementById("num").value;
+//     let array = [];
+//     if (arraySize > 0 && arraySize <= 100) {
+//         for (let i = 0; i < arraySize; i++) {
+//             array.push(Math.floor(Math.random()*100));
+//         }
+//         document.getElementById("show").innerHTML = `Arreglo generado: <br><br>[${array}]`;
+//         document.getElementById("search-box").style.display = "block";
+//     } else {
+//         document.getElementById("show").innerHTML = `El número no es aceptable`;
+//     }
+//     return arraySize;
+// }
+
+// Buscar número 
+// function searchNumber() {
+//     var arraySize = document.getElementById("num").value;
+//     let array = [];
+//     let searchNum = document.getElementById("search").value;
+//     if (searchNum >= 0 && searchNum <= 100) {
+//         for (let i = 0; i < arraySize; i++) {
+//             for (let j = 0; j < arraySize; j++) {
+//                 if (i == array[j]) {
+//                     console.log("adios" + j);
+//                     document.getElementById("show-element").innerHTML = `El número ${searchNum} está en la posición ${j}`;
+//                 }
+//             }
+//         }
+//     } else {
+//         document.getElementById("show-element").innerHTML = `El número ${searchNum} no está en el arreglo`;
+//     }
+// }
